270db70c1786d8571232003f3b278e9ea8b39ed8
Dane Stuckel <dane.stuckel@gmail.com>
2024-01-10 21:52:16 +0700

    Better docs and ease of use

--- common-s3-headers/src/lib.rs
+++ common-s3-headers/src/lib.rs
@@ -2,12 +2,8 @@ pub mod aws_canonical;
 pub mod aws_format;
 pub mod aws_math;
 mod s3;
-mod s3_options;
 
-pub use aws_format::*;
-pub use aws_math::get_sha256;
 pub use s3::*;
-pub use s3_options::*;
 
 #[cfg(test)]
 mod tests;
--- common-s3-headers/tests/integration/test_put_object.rs
+++ common-s3-headers/tests/integration/test_put_object.rs
@@ -1,5 +1,5 @@
 use crate::test_util::{assert, setup};
-use common_s3_headers::{self, S3Options};
+use common_s3_headers::{self, S3HeadersBuilder};
 use std::str::FromStr;
 use url::Url;
 
@@ -9,18 +9,81 @@ fn test_put_object() {
 
   let url = Url::from_str("https://jsonlog.s3.amazonaws.com/test/test2.json").unwrap();
   let content = "{\"c\":\"d\"}\n".as_bytes().to_vec();
-  let sha = common_s3_headers::get_sha256(&content);
-  let options = S3Options::new(&url)
+  let sha = common_s3_headers::aws_math::get_sha256(&content);
+  let headers = S3HeadersBuilder::new(&url)
     .set_access_key(&access_key)
     .set_secret_key(&secret_key)
     .set_region(&region)
     .set_method("PUT")
     .set_service("s3")
-    .set_payload_hash(&sha);
-
-  let headers: Vec<(&str, String)> = common_s3_headers::get_headers(options);
+    // Only borrowed should work.
+    .set_payload_hash(&sha)
+    .build();
 
   let (status_code, _, body) = assert::request_put(url, headers, content);
   assert::equal(status_code, 200);
   assert::equal(body, "");
+}
+
+// Should be able to set this as const.
+const PUT_METHOD: &str = "PUT";
+
+#[test]
+fn test_put_object_with_sha_shortcut() {
+  let (access_key, secret_key, region) = setup::get_config_from_env("TEST_S3");
+
+  let url = Url::from_str("https://jsonlog.s3.amazonaws.com/test/test2.json").unwrap();
+  let content = "{\"c\":\"d\"}\n".as_bytes().to_vec();
+  let headers = S3HeadersBuilder::new(&url)
+    .set_access_key(&access_key)
+    .set_secret_key(&secret_key)
+    .set_region(&region)
+    // This const should be okay.
+    .set_method(PUT_METHOD)
+    .set_service("s3")
+    // This creates a new payload value that is owned until dropped.
+    .set_payload_hash_with_content(&content)
+    .build();
+
+  let (status_code, _, body) = assert::request_put(url, headers, content);
+  assert::equal(status_code, 200);
+  assert::equal(body, "");
+}
+
+#[test]
+fn test_put_object_full() {
+  // Build the S3 headers
+  let (access_key, secret_key, region) = setup::get_config_from_env("TEST_S3");
+  let url = Url::from_str("https://jsonlog.s3.amazonaws.com/test/test2.json").unwrap();
+  let content = "{\"c\":\"d\"}\n".as_bytes().to_vec();
+  let headers = S3HeadersBuilder::new(&url)
+    .set_access_key(&access_key)
+    .set_secret_key(&secret_key)
+    .set_region(&region)
+    // This const should be okay.
+    .set_method(PUT_METHOD)
+    .set_service("s3")
+    // This creates a new payload value that is owned until dropped.
+    .set_payload_hash_with_content(&content)
+    .build();
+
+  // Use Reqwest
+  let response = reqwest::blocking::Client::new()
+    .get(url)
+    .headers(reqwest::header::HeaderMap::from_iter(headers.into_iter().map(
+      |(k, v)| {
+        (
+          reqwest::header::HeaderName::from_str(k).unwrap(),
+          reqwest::header::HeaderValue::from_str(&v).unwrap(),
+        )
+      },
+    )))
+    .send()
+    .unwrap();
+
+  // Print results
+  let status_code = response.status();
+  let response_headers = response.headers().clone();
+  let body = response.text().unwrap();
+  println!("{}\n{:#?}\n{}", status_code, response_headers, body);
 }
